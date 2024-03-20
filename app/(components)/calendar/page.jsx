03d4a5ddf199fd8b0e9ad10017392ad172643761
Codemaverick07 <hemantjatal59@gmail.com>
2024-03-21 02:43:49 +0530

    first commit

---
+++ app/(components)/calendar/page.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Cal_1 from "./calendar/calendar-1.react.jsx";
+import Cal_2 from "./calendar/calendar-2.react.jsx";
+import Cal_3 from "./calendar/calendar-3.react.jsx";
+import Cal_4 from "./calendar/calendar-4.react.jsx";
+import Cal_5 from "./calendar/calendar-5.react.jsx";
+import Cal_6 from "./calendar/calendar-6.react.jsx";
+import Cal_7 from "./calendar/calendar-7.react.jsx";
+import Cal_8 from "./calendar/calendar-8.react.jsx";
+import Cal_9 from "./calendar/calendar-9.react.jsx";
+import Cal_10 from "./calendar/calendar-10.react.jsx";
+import Cal_11 from "./calendar/calendar-11.react.jsx";
+import Cal_12 from "./calendar/calendar-12.react.jsx";
+import Cal_13 from "./calendar/calendar-with-stretched-task.react.jsx";
+import Cal_14 from "./calendar/date-picker.react.jsx";
+import Theme from "@/components/Theme.jsx";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="text-center text-xl font-bold">1st</div>
+      <Cal_1 className="h-full" />
+      <div className="text-center text-xl font-bold">2nd</div>
+      <Cal_2 className="h-full" />
+      <div className="text-center text-xl font-bold">3rd</div>
+      <Cal_3 className="h-full" />
+      <div className="text-center text-xl font-bold">4th</div>
+      <Cal_4 className="h-full" />
+      <div className="text-center text-xl font-bold">5th</div>
+      <Cal_5 className="h-full" />
+      <div className="text-center text-xl font-bold">6th</div>
+      <Cal_6 className="h-full" />
+      <div className="text-center text-xl font-bold">7th</div>
+      <Cal_7 className="h-full" />
+      <div className="text-center text-xl font-bold">8th</div>
+      <Cal_8 className="h-full" />
+      <div className="text-center text-xl font-bold">9th</div>
+      <Cal_9 className="h-full" />
+      <div className="text-center text-xl font-bold">10th</div>
+      <Cal_10 className="h-full" />
+      <div className="text-center text-xl font-bold">11th</div>
+      <Cal_11 className="h-full" />
+      <div className="text-center text-xl font-bold">12th</div>
+      <Cal_12 className="h-full" />
+      <div className="text-center text-xl font-bold">13th</div>
+      <Cal_13 className="h-full" />
+      <div className="text-center text-xl font-bold">14th</div>
+      <Cal_14 className="h-full" />
+    </main>
+  );
+}
---
+++ app/(components)/feedback/page.jsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import Ale1 from "./alert/alert-1.react";
+import Ale2 from "./alert/alert-2.react";
+import Ale3 from "./alert/alert-3.react";
+import Ale4 from "./alert/alert-4.react";
+import Ale5 from "./alert/alert-5.react";
+import Ale6 from "./alert/alert-6.react";
+import Ale7 from "./alert/alert-7.react";
+import Ale8 from "./alert/alert-8.react";
+import Ale9 from "./alert/alert-9.react";
+import Ale10 from "./alert/alert-10.react";
+import Ale11 from "./alert/alert-11.react";
+import Ale12 from "./alert/alert-12.react";
+import Ale13 from "./alert/alert-13.react";
+import Ale14 from "./alert/alert-14.react";
+import Ale15 from "./alert/alert-15.react";
+import Ale16 from "./alert/alert-16.react";
+import Ale17 from "./alert/alert-17.react";
+import Ale18 from "./alert/alert-18.react";
+import Ale19 from "./alert/alert-19.react";
+import Ale20 from "./alert/alert.react";
+import Ale21 from "./alert/color-coded-with-icon-error.react";
+import Ale22 from "./alert/color-coded-with-icon-info.react";
+import Ale23 from "./alert/color-coded-with-icon-success.react";
+import Ale24 from "./alert/color-coded-with-icon-warning.react";
+import Ale25 from "./alert/fixed-on-top-error.react";
+import Ale26 from "./alert/fixed-on-top-info.react";
+import Ale27 from "./alert/fixed-on-top-success.react";
+import Ale28 from "./alert/fixed-on-top-warning.react";
+
+import Ale30 from "./alert/simple-with-action-button-info.react";
+import Ale31 from "./alert/simple-with-action-button-success.react";
+import Ale32 from "./alert/simple-with-action-button-warning.react";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="">
+      <div className="text-center text-xl font-bold">1</div>
+      <Ale1 />
+      <div className="text-center text-xl font-bold">2</div>
+      <Ale2 />
+      <div className="text-center text-xl font-bold">3</div>
+      <Ale3 />
+      <div className="text-center text-xl font-bold">4</div>
+      <Ale4 />
+      <div className="text-center text-xl font-bold">5</div>
+      <Ale5 />
+      <div className="text-center text-xl font-bold">6</div>
+      <Ale6 />
+      <div className="text-center text-xl font-bold">7</div>
+      <Ale7 />
+      <div className="text-center text-xl font-bold">8</div>
+      <Ale8 />
+      <div className="text-center text-xl font-bold">9</div>
+      <Ale9 />
+      <div className="text-center text-xl font-bold">10</div>
+      <Ale10 />
+      <div className="text-center text-xl font-bold">11</div>
+      <Ale11 />
+      <div className="text-center text-xl font-bold">12</div>
+      <Ale12 />
+      <div className="text-center text-xl font-bold">13</div>
+      <Ale13 />
+      <div className="text-center text-xl font-bold">14</div>
+      <Ale14 />
+      <div className="text-center text-xl font-bold">15</div>
+      <Ale15 />
+      <div className="text-center text-xl font-bold">16</div>
+      <Ale16 />
+      <div className="text-center text-xl font-bold">17</div>
+      <Ale17 />
+      <div className="text-center text-xl font-bold">18</div>
+      <Ale18 />
+      <div className="text-center text-xl font-bold">19</div>
+      <Ale19 />
+      <div className="text-center text-xl font-bold">20</div>
+      <Ale20 />
+      <div className="text-center text-xl font-bold">21</div>
+      <Ale21 />
+      <div className="text-center text-xl font-bold">22</div>
+      <Ale22 />
+      <div className="text-center text-xl font-bold">23</div>
+      <Ale23 />
+      <div className="text-center text-xl font-bold">24</div>
+      <Ale24 />
+      <div className="text-center text-xl font-bold">25</div>
+      <Ale25 />
+      <div className="text-center text-xl font-bold">26</div>
+      <Ale26 />
+      <div className="text-center text-xl font-bold">27</div>
+      <Ale27 />
+      <div className="text-center text-xl font-bold">28</div>
+      <Ale28 />
+
+      <div className="text-center text-xl font-bold">30</div>
+      <Ale30 />
+      <div className="text-center text-xl font-bold">31</div>
+      <Ale31 />
+      <div className="text-center text-xl font-bold">32</div>
+      <Ale32 />
+    </main>
+  );
+};
+
+export default page;
